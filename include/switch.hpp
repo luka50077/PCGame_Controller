9a4b63036d04b6ab9ae1d564488f0503029d3d27
thyristor <nishizawa-yutaka006@g.ecc.u-tokyo.ac.jp>
2026-02-06 20:52:10 +0900

    v2

--- include/switch.hpp
+++ include/switch.hpp
@@ -9,9 +9,6 @@ constexpr int SWITCH_NUM = 10;
 
 class Switch {
  private:
-  const int ONE_SHOT_TIME = 10;
-  std::array<bool, SWITCH_NUM> oneshot_state = {};
-  std::array<int, SWITCH_NUM> oneshot_cnt = {};
   const std::array<uint16_t, SWITCH_NUM> gpio_pins = {
       Escape_Pin,     SW2_Pin, SW1_Pin, SW0_Pin, SpaceL_Pin,
       Encoder_SW_Pin, SW3_Pin, SW4_Pin, SW5_Pin, SpaceR_Pin};
--- src/switch.cpp
+++ src/switch.cpp
@@ -2,18 +2,6 @@
 
 void Switch::read() {
   for (int i = 0; i < SWITCH_NUM; i++) {
-    bool pressed = (HAL_GPIO_ReadPin(gpio_ports[i], gpio_pins[i]) == GPIO_PIN_RESET);
-
-    if (pressed && !state[i]) {
-      oneshot_state[i] = true;
-      oneshot_cnt[i] = ONE_SHOT_TIME;
-    }
-    state[i] = pressed;
-    if (oneshot_cnt[i] > 0) {
-      oneshot_cnt[i]--;
-      if (oneshot_cnt[i] == 0) {
-        oneshot_state[i] = false;
-      }
-    }
+    state[i] = (HAL_GPIO_ReadPin(gpio_ports[i], gpio_pins[i]) == GPIO_PIN_RESET);
   }
 }
